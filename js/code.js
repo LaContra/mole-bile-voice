7b42e88e510b0cf2eb10fd26266338e5a936a9ab
jingwan <hsugaigai@gmail.com>
2016-04-09 19:25:17 +0100

    create Intent button -> create empty intent select one intent -> edit info select any one or more intent or edge -> press delete -> delete element automatically save and load elements at local storage

--- js/code.js
+++ js/code.js
@@ -1,137 +1,163 @@
 // $(function(){ // on dom ready
 
-var cy = cytoscape({
-  container: document.getElementById('cy'),
-  
-  boxSelectionEnabled: false,
-  selectionType: 'single',
-  autounselectify: false,
-  minZoom: 1,
-  maxZoom: 10,
+function saveElements() {
+    localStorage.setItem("elements", JSON.stringify(cy.json().elements));
+}
 
-  style: [
-    {
-      selector: 'node',
-      css: {
-        'content': 'data(name)',
-        'text-valign': 'center',
-        'text-halign': 'center'
-      }
-    },
-    {
-      selector: '$node > node',
-      css: {
-        'padding-top': '10px',
-        'padding-left': '10px',
-        'padding-bottom': '10px',
-        'padding-right': '10px',
-        'text-valign': 'top',
-        'text-halign': 'center',
-        'background-color': '#bbb'
-      }
-    },
-    {
-      selector: 'edge',
-      css: {
-        'target-arrow-shape': 'triangle',
-        'content': 'data(id)'
-      }
-    },
-    {
-      selector: ':selected',
-      css: {
-        'background-color': 'black',
-        'line-color': 'black',
-        'target-arrow-color': 'black',
-        'source-arrow-color': 'black'
-      }
+function getElements() {
+    elements = localStorage.getItem("elements");
+    if (elements && elements != "{}") {
+        return JSON.parse(elements);
     }
-  ],
-  
-  elements: {
-    nodes: [
-      { data: { id: 'aaaaaaa', yaya: 'test', name: 'syayayaya' }, position: { x: 215, y: 85 } },
-      { data: { id: 'c', name: 'yy' }, position: { x: 300, y: 85 } },
-      { data: { id: 'd' }, position: { x: 215, y: 175 } },
-      { data: { id: 'f' }, position: { x: 300, y: 175 } }
-    ],
-    edges: [
-      { data: { id: 'ad', source: 'aaaaaaa', target: 'd' } }
-      // { data: { id: 'eb', source: 'e', target: 'b' } }
-      
-    ]
-  },
-  
-  layout: {
-    name: 'preset',
-    padding: 5
-  }
+    else {
+        return null;
+    }
+}
+var cy = cytoscape({
+    container: document.getElementById('cy'),
+
+    boxSelectionEnabled: false,
+    selectionType: 'additive',
+    autounselectify: false,
+    minZoom: 1,
+    maxZoom: 10,
+
+    style: [{
+        selector: 'node',
+        css: {
+            'content': 'data(user_says)',
+            'text-valign': 'center',
+            'text-halign': 'center',
+            'color': 'white',
+            'text-outline-width': 2,
+            'text-outline-color': '#888',
+            'shape': 'rectangle',
+        }
+    }, {
+        selector: '$node > node',
+        css: {
+            'padding-top': '10px',
+            'padding-left': '10px',
+            'padding-bottom': '10px',
+            'padding-right': '10px',
+            'text-valign': 'top',
+            'text-halign': 'center',
+            'background-color': '#bbb'
+        }
+    }, {
+        selector: 'edge',
+        css: {
+            'target-arrow-shape': 'triangle',
+            // 'content': 'data(id)'
+        }
+    }, {
+        selector: ':selected',
+        css: {
+            'background-color': 'black',
+            'line-color': 'black',
+            'target-arrow-color': 'black',
+            'source-arrow-color': 'black',
+            'text-outline-color': 'black',
+        }
+    }],
+    elements: getElements(),
+    layout: {
+        name: 'preset',
+        padding: 5
+    }
 });
 
-cy.on('select', 'node', function(evt){
-  var node = evt.cyTarget;
-  console.log( 'select ' + node.id() );
+// save elements to localstorage
+cy.on('add, remove, position, data', function (evt) {
+    saveElements();
 });
 
-cy.on('select', 'edge', function(evt){
-  var edge = evt.cyTarget;
-  console.log( 'select ' + edge.id() );
-});
 
+/* Edge control */
 
 var cxtdragStart = false;
-var edgeFrom = null, edgeTo = null;
+var edgeFrom = null,
+    edgeTo = null;
 
-cy.on('cxtdrag', 'node', function(evt){
-  var node = evt.cyTarget;
-  // console.log( 'cxtdrag ' + node.id() );
-  cxtdragStart = true;
-  edgeFrom = node;
+cy.on('cxtdrag', 'node', function(evt) {
+    var node = evt.cyTarget;
+    // console.log( 'cxtdrag ' + node.id() );
+    cxtdragStart = true;
+    edgeFrom = node;
 });
-cy.on('cxtdragover', 'node', function(evt){
-  var node = evt.cyTarget;
-  // console.log( 'cxtdragover ' + node.id() );
-  if (!cxtdragStart || node == edgeFrom) {
-    return;
-  }
-  edgeTo = node;
+cy.on('cxtdragover', 'node', function(evt) {
+    var node = evt.cyTarget;
+    if (!cxtdragStart || node == edgeFrom) {
+        return;
+    }
+    edgeTo = node;
 });
-cy.on('cxtdragout', 'node', function(evt){
-  var node = evt.cyTarget;
-  // console.log( 'cxtdragout ' + node.id() );
-  if (!cxtdragStart || node != edgeTo) {
-    return
-  }
-  edgeTo = null;
+cy.on('cxtdragout', 'node', function(evt) {
+    var node = evt.cyTarget;
+    if (!cxtdragStart || node != edgeTo) {
+        return
+    }
+    edgeTo = null;
 });
-cy.on('cxttapend', 'node', function(evt){
-  var node = evt.cyTarget;
-  // console.log( 'cxttapend ' + node.id() );
-  if (!cxtdragStart) {
-    return
-  }
-  cxtdragStart = false;
-  if (edgeTo == null) {
-    return
-  }
-  // console.log(edgeFrom.id(), edgeTo.id());
-  cy.add({
-    group: "edges",
-    data: { source: edgeFrom.id(), target: edgeTo.id() }
-  });
-
-  edgeTo = null;
+cy.on('cxttapend', 'node', function(evt) {
+    var node = evt.cyTarget;
+    if (!cxtdragStart) {
+        return
+    }
+    cxtdragStart = false;
+    if (edgeTo == null) {
+        return
+    }
+    cy.add({
+        group: "edges",
+        data: { source: edgeFrom.id(), target: edgeTo.id() }
+    });
+    edgeTo = null;
 });
 
-function addNode() {
+/* panel control */
+
+// add intent
+$("#add_intent_btn").click(function() {
+    cy.$(":selected").unselect();
     cy.add({
         group: "nodes",
-        data: { speech: $("#user_say").val()+ " - " + $("#response").val(), position: { x: 0, y: 0 } }
-    });
-}
-$("#delete_btn").click(function() {
-    cy.remove(':selected');
+        data: { user_says: "", response: "" },
+        position: { x: -cy.viewport().pan().x / cy.zoom() + 40, y: -cy.viewport().pan().y / cy.zoom() + 40 }
+    }).select();
 });
 
+
+// delete intent or edge
+$('body').keydown(function(event) {
+    if ($("input[type='text']:focus").length > 0) {
+        return;
+    }
+    var key = event.which || event.keyCode; // event.keyCode is used for IE8 and earlier
+    if (key == 8) {
+        cy.$(':selected').unselect().remove();
+    }
+})
+
+// show or hide intent info editor
+cy.on('select, unselect', 'node, edge', function(evt) {
+    if (cy.$(":selected").length == 1 && cy.$("node:selected").length == 1) {
+        var node = cy.$("node:selected")[0];
+        $("#intent_info #user_says").val(node.data("user_says"));
+        $("#intent_info #response").val(node.data("response"));
+        $("#intent_info").show();
+    }
+    else {
+        $("#intent_info").hide();
+    }
+});
+
+// modify intent info
+$("#intent_info").submit(function() {
+    cy.$("node:selected").data({
+        user_says: $("#intent_info #user_says").val(),
+        response: $("#intent_info #response").val()
+    }).unselect();
+});
 
 // }); // on dom ready
