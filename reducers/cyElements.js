79c22b0d4c1abed75692759f96c021fa2bbf3373
jingwan <hsugaigai@gmail.com>
2016-04-20 01:43:36 +0100

    SEND_CREATE_INTENT_REQUEST structure modified

--- reducers/cyElements.js
+++ reducers/cyElements.js
@@ -12,21 +12,38 @@ const modifyElement = (element, targetId, userSays, response) => {
   return Object.assign({}, element, {data: {USER_SAYS: userSays, RESPONSE: response, id: targetId}})
 }
 
-const getContextName = (edge) => {
+const filterNode = (element) => {
+  return element.group == "nodes"
+}
+
+const filterEdge = (element) => {
+  return element.group == "edges"
+}
+
+const getContextNameFromEdge = (edge) => {
   return edge.data.source + "_to_" + edge.data.target
+}
+
+const getEdgesIn = (node, state) => {
+  return state.filter(filterEdge).filter(e => { 
+      return e.data.target == node.data.id
+  })
+}
+
+const getEdgesOut = (node, state) => {
+  return state.filter(filterEdge).filter(e => { 
+      return e.data.source == node.data.id
+  })
 }
 
 const buildIntentsDataFromCyElements = (state) => {
   // for every nodes
-  return state.filter(e => { return e.group == "nodes" }).map(node => {
-    // find inbound edges
-    const contextsIn = state.filter(e => { 
-      return e.group == "edges" && e.data.target == node.data.id
-    }).map(getContextName)
-    // find outbound edges
-    const contextsOut = state.filter(e => { 
-      return e.group == "edges" && e.data.source == node.data.id
-    }).map(getContextName)
+  return state.filter(filterNode).map(node => {
+    const contextsIn = getEdgesIn(node, state).map(getContextNameFromEdge)
+    const contextsOut = getEdgesOut(node, state).map(getContextNameFromEdge)
+
+    console.log(contextsIn)
+    console.log(contextsOut)
 
     return {
       name: node.data.id,
@@ -42,18 +59,16 @@ const buildIntentsDataFromCyElements = (state) => {
   })
 }
 
-const sendCreateIntentRequest = (intentsData) => {
-  intentsData.map(data => {
-    $.ajax({
-        url: "https://api.api.ai/v1/intents?v=20160416",
-        beforeSend: function (request) {
-            request.setRequestHeader("Authorization", "Bearer key");
-        },
-        type: "POST",
-        data: JSON.stringify(data),
-        contentType: "application/json",
-        complete: function(e) { console.log(e)}
-    });
+const sendCreateIntentRequest = (intentData) => {
+  $.ajax({
+      url: "https://api.api.ai/v1/intents?v=20160416",
+      beforeSend: function (request) {
+          request.setRequestHeader("Authorization", "Bearer key");
+      },
+      type: "POST",
+      data: JSON.stringify(intentData),
+      contentType: "application/json",
+      complete: function(e) { console.log(e)}
   })
 }
 
@@ -93,7 +108,7 @@ const cyElements = (state, action) => {
     // create intent 
     case 'SEND_CREATE_INTENT_REQUEST':
       console.log("send create intent request");
-      sendCreateIntentRequest(buildIntentsDataFromCyElements(state))
+      buildIntentsDataFromCyElements(state).forEach(sendCreateIntentRequest)
       return state
 
     default:
