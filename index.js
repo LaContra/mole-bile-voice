384ed5043089e46d8f841152d5c865a3db0a55e3
Pi-Tan Hu <lacontrazania@gmail.com>
2016-04-26 21:03:29 -0700

    basic setup for async actions: thunk, es6 promise and fetch

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+import 'babel-polyfill'
+import thunkMiddleware from 'redux-thunk'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import voiceUIApp from './common/reducers'
 import App from './common/App'
 
-let store = createStore(voiceUIApp)
+let store = createStore(
+  voiceUIApp,
+  applyMiddleware(
+    thunkMiddleware
+  )
+)
 
 render(
   <Provider store={store}>
