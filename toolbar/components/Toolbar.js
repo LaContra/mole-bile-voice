5b5898cb560863391fc126fb26feb3c340708dfa
Pi-Tan Hu <lacontrazania@gmail.com>
2016-05-05 22:45:48 -0700

    Add a website title

--- toolbar/components/Toolbar.js
+++ toolbar/components/Toolbar.js
@@ -4,6 +4,7 @@ import IntentRequest from '../containers/IntentRequest'
 const Toolbar = () => (
   <div className="toolbar">
     <img className='logo' src={'/common/images/logo-design-mid-res.png'} />
+    <h2 className='web-title'>Mole-bile Voice</h2>
     <IntentRequest/>
   </div>
 )
